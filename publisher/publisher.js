8e574472f2c14f57a997098568b51080737c92f6
Shruti Jain <sh2000.cool.j@gmail.com>
2022-05-04 02:01:30 +0530

    basic logic added

--- publisher/publisher.js
+++ publisher/publisher.js
@@ -1,13 +1,13 @@
 const amqp = require('amqplib');
 connect();
+const msg = '1fg582 234930';
 async function connect() {
 try{
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
     const result =  await channel.assertQueue("jobs");
-    channel.sendToQueue("jobs", Buffer.from("Hello World"));
-    console.log(" [x] Sent 'Hello World'");
-}
+    channel.sendToQueue("jobs", Buffer.from(msg));
+    }
 catch(ex){
    console.log(ex);
 }
--- subscriber/subscriber.js
+++ subscriber/subscriber.js
@@ -1,12 +1,27 @@
 const amqp = require('amqplib');
+
 connect();
+let vehicle = new Map();
 async function connect() {
 try{
     const connection = await amqp.connect("amqp://localhost:5672");
     const channel = await connection.createChannel();
-    const result =  await channel.assertQueue("jobs");
+    const result =  await channel.assertQueue("jobs"); 
     channel.consume("jobs", message => {
-        console.log(message.content.toString());
+        let msg = message.content.toString().split(' ');
+        if(!vehicle.has(msg[0])){
+            vehicle.set(msg[0],msg[1]);
+            channel.ack(message);
+        }
+        else{
+            let t1= vehicle.get(msg[0]);
+            let t2= msg[1];
+            time = t2-t1;
+            console.log(time);
+            console.log(vehicle);
+            vehicle.delete(msg[0]);
+            channel.ack(message);
+        }
     });
     console.log("Waiting for the messages.....");
 }
